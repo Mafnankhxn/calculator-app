4027d8fa8d8bece502599f66e386d300fb953eaa
muhammad <you@example.com>
2023-09-19 07:13:10 -0700

    made a simple calculator

---
+++ script.js
@@ -0,0 +1,20 @@
+let string = "";
+const buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button) => {
+button.addEventListener('click', (e) => {
+    console.log(e.target.innerHTML)
+    if(e.target.innerHTML === '='){
+        string = eval(string);
+        document.querySelector('.input').value = string;
+    }
+    else if(e.target.innerHTML === 'C' || e.target.innerHTML === 'AC'){
+        string = '';
+        document.querySelector('.input').value = string;
+    }
+    else{
+        string = string + e.target.innerHTML;
+        document.querySelector('.input').value = string;
+    }
+})
+})
+
